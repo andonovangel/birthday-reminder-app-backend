3607a620155117ddc9563a7874cbd24d851f2ad1
Angel Andonov <andonovangel1@gmail.com>
2023-09-17 13:28:10 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\GroupController;
 use App\Http\Controllers\BirthdayController;
 use App\Http\Controllers\MailController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
